9c891804136f2d1dcea24df4a9c94ea249bc05b3
Lucas Von der Heyde <lucasvonderheyde@gmail.com>
2023-10-18 17:47:49 -0400

    board now printing properly

--- main.py
+++ main.py
@@ -2,27 +2,19 @@ from classes.Row import Row
 from classes.Square import Square
 from classes.Player import Player
 
-#three rows
-row_1 = Row(0)
-row_2 = Row(1)
-
-player_1 = Player("Nav")
-
-#row 1 square values
-square_1 = Square(row_1, player_1, 0)
-square_2 = Square(row_1, player_1, 1)
-square_3 = Square(row_1, player_1, 2)
-
-square_4 = Square(row_2, player_1, 0)
-square_5 = Square(row_2, player_1, 1)
-square_6 = Square(row_2, player_1, 2)
-
-
 def main():
 
+    player_name = input("what is your name?")
+    board_size = input("how many rows and columns do you want?")
+
+    player = Player(player_name)
+
+    board = board_creator(board_size, player)
+
+
     while True:
-        print(row_1.squares)
-        print(row_2.squares)
+
+        print_board(board)
 
         player_row_input = input('Which row do you want?')
 
@@ -37,13 +29,30 @@ def main():
                 change_square_value = input("input your choice")
                 square.value = change_square_value
 
-        print(row_1.squares)
-        print(row_2.squares)
 
         quit_out = input("end")
 
         if quit_out == "y":
             return False
+
+
+def board_creator(board_size, player):
+    board = []
+    i = 0
+    while i < int(board_size):
+        row = Row(i)
+        x = 0
+        while x < int(board_size):
+            Square(row, player, x)
+            x += 1
+        i += 1
+        board.append(row.squares)
+    return board
+
+def print_board(board):
+    for row in board:
+        print(row)      
+
 
 if __name__ == '__main__':
     main()
